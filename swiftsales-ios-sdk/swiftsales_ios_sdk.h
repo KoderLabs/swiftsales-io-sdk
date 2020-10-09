0ad1078b97b6aef2672311a2bddf658a16db2fc1
anum shafiq <anum.shafiq@koderlabs.com>
2020-10-09 17:02:19 +0500

    Initial Commit

---
+++ swiftsales-ios-sdk/swiftsales_ios_sdk.h
@@ -0,0 +1,19 @@
+//
+//  swiftsales_ios_sdk.h
+//  swiftsales-ios-sdk
+//
+//  Created by Koderlabs on 09/10/2020.
+//  Copyright © 2020 Swiftsales. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swiftsales_ios_sdk.
+FOUNDATION_EXPORT double swiftsales_ios_sdkVersionNumber;
+
+//! Project version string for swiftsales_ios_sdk.
+FOUNDATION_EXPORT const unsigned char swiftsales_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftsales_ios_sdk/PublicHeader.h>
+
+
